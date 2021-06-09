0543943cd6a0f2fc4dc26b9dff2537428b7e74a0
AyyyStew <alexsteward98@gmail.com>
2021-06-09 17:20:44 -0500

    moved website html pages into webstie directory this was done to seperate out the ui from the api and to avoid issues with mounting files at the website root

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from os import name
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
+from os.path import isfile
 
-
+from fastapi import FastAPI, WebSocket
 from security import auth
+from website import site
 
 app = FastAPI()
 app.include_router(auth.router)
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.include_router(site.router)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print(data)
+        await websocket.send_text(f"Message text was: {data}")
+
+
---
+++ app/website/site.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from starlette.requests import Request
+
+router = APIRouter()
+
+# These paths seem fragile
+router.mount("/static", StaticFiles(directory="./website/static", html=True), name="static")
+templates = Jinja2Templates(directory="./website/templates")
+
+@router.get("/")
+@router.get("/index.html")
+async def login(request: Request):
+    return templates.TemplateResponse("./index.html", context={'request': request})
+
+
+@router.get("/signup")
+@router.get("/signup.html")
+async def signup(request: Request):
+    return templates.TemplateResponse("./signup.html", context={'request': request})
+
+@router.get("/home")
+@router.get("/home.html")
+async def home(request: Request):
+    return templates.TemplateResponse("./home.html", context={'request': request})
