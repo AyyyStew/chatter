fa4510323023ffea127139498688f60d96023f8f
AyyyStew <alexsteward98@gmail.com>
2021-06-08 22:31:12 -0500

    mounted static files to root. changed login to index to serve it as root

--- app/main.py
+++ app/main.py
@@ -1,28 +1,14 @@
+from os import name
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
 
 from security import auth
 
-class Item(BaseModel):
-	name: str
-	description: Optional[str] = None
-	price: float
-	tax: Optional[float] = None
-
-
 app = FastAPI()
 app.include_router(auth.router)
-
-@app.get("/")
-async def root():
-	return {"message": "hello world"}
-
-
-@app.get("/{number}")
-async def number(number: int):
-	return {"number" : number}
-
-@app.post("/items/")
-async def create_item(item: Item):
-	return item
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
