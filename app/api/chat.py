f05aca6eee3355eb9c0f3320a389d60bce739521
AyyyStew <alexsteward98@gmail.com>
2021-06-09 17:45:25 -0500

    started websocket chat

---
+++ app/api/chat.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, WebSocket
+
+router = APIRouter(prefix="/api")
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print(data)
+        await websocket.send_text(f"Message text was: {data}")
+
--- app/main.py
+++ app/main.py
@@ -4,21 +4,17 @@ from os.path import isfile
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 
+from api import chat
 from security import auth
 from website import site
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(site.router)
+app.include_router(chat.router)
 
 app.mount("/static", StaticFiles(directory="./static/", html=True), name="static")
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        print(data)
-        await websocket.send_text(f"Message text was: {data}")
+
 
 
