45048c3f65bd8368fb75b509a003336ac540b86a
AyyyStew <alexsteward98@gmail.com>
2021-06-09 14:53:56 -0500

    added error message for better ui

--- app/static/src/login.js
+++ app/static/src/login.js
@@ -1,12 +1,10 @@
-const login = (username, password) =>{
+const login = async (username, password) =>{
 	let params = new URLSearchParams({
 		username,
 		password,
 	})
 
-	console.log(params)
-
-	fetch("/auth/login", {
+	return await fetch("/auth/login", {
 		body: params,
 		headers: {
 		  Accept: "application/json",
@@ -26,22 +24,43 @@ const login = (username, password) =>{
 	})
 	.then(data => {
 		console.log('Success:', data);
+		// set api token in local storage
 		sessionStorage.setItem("CHATTER_TOKEN", data.access_token)
-		window.location.replace("home.html")
+
+		// send user to homepage
+		window.location.href = "home.html"
+		
+		//return success message if login takes a bit
+		return {status: 200, message : "Success!"}
 	})
 	.catch((error) => {
 		console.error('Error:', error);
+		let result = {status : error.status}
+		if (error.status == 401){
+			result.message = "Incorrect Username or Password"
+		} else{
+			result.message = "There was an Error. Please try again Later"
+		}
+
+		return result
+
+
 	});
 }
 
 loginForm = document.getElementById("login-form")
-loginForm.addEventListener("submit", (e) =>{
+loginForm.addEventListener("submit", async(e) => {
 	e.preventDefault()
 	requestBody = {}
 	for (element of loginForm.querySelectorAll("input")){
 		requestBody[element.id] = element.value
 	}
 	
-	login(requestBody["username"], requestBody["password"])
-
+	const result = await login(requestBody["username"], requestBody["password"])
+	console.log(result)
+	const statusMessage = loginForm.querySelector(".status-message")
+	statusMessage.innerHTML = result.message
+	
+	result.status == 200 ? statusMessage.classList.add("success") : statusMessage.classList.add("error")
+	
 })
