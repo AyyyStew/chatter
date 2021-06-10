02c6882d2141b8c049c38a78b8d818778a34d12c
AyyyStew <alexsteward98@gmail.com>
2021-06-10 15:31:44 -0500

    extracted datetime serialization logic into class

--- app/models.py
+++ app/models.py
@@ -10,6 +10,12 @@ class Message(BaseModel):
 	timestamp: datetime
 	user: User
 
+	def getJSONSafeMapping(self):
+		# this is here to eliminate datetime serialization issues
+		# pydantic probably has a solution but i couldn't find it
+		return json.loads(self.json())
+
+
 def get_db_table_instance(table):
     db = TinyDB(f"./databases/chatter.json")
     table = db.table(table)
@@ -20,4 +26,4 @@ def store_message(user: User, message: str, chatroomID: str):
 	chatroomTable = get_db_table_instance(f"{chatroomID}")
 	record : Message = Message(chatroomID=chatroomID, message=message, timestamp=datetime.now(), user=user)
 
-	chatroomTable.insert(json.loads(record.json()))
+	chatroomTable.insert(record.getJSONSafeMapping())
