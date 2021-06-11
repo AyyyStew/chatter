03c63759df851ada268bfc5272ef68f9e03ac957
AyyyStew <alexsteward98@gmail.com>
2021-06-11 14:25:56 -0500

    flattened if block. added comments

--- app/chat.py
+++ app/chat.py
@@ -22,10 +22,15 @@ class ChatRoom:
         self.chatConnections.append(websocket)
 
     async def broadcast(self, message: Message):
-        messageWithoutChatroomID = message.getJSONSafeMapping()
-        messageWithoutChatroomID.pop("chatroomID")
+        messageDict = message.getJSONSafeMapping()
+        
+        # remove items before we send it over the network for privacy and bandwidth
+        messageDict.pop("chatroomID")
+        messageDict["user"].pop("full_name")
+
+        # send message to all websockets in connection pool
         for connection in self.chatConnections:
-            await connection.send_json(messageWithoutChatroomID)
+            await connection.send_json(messageDict)
         
     def disconnect(self, websocket):
         self.chatConnections.remove(websocket)
@@ -38,24 +43,22 @@ globalChat = ChatRoom(roomID=1)
 # probably will be fixed later https://github.com/tiangolo/fastapi/pull/3280
 @router.websocket(f"{prefix}/chat")
 async def websocket_endpoint(websocket: WebSocket, token: str = Query(...)):
-    print(token)
     # TODO Multiple Chatrooms
     chatroomID = 1
 
     # remove hashed password from data we are going to send
     userWithTooManyDetails = await get_current_user(token)
     user : User =  User(**userWithTooManyDetails.dict())
-    
-    # if authenticated
-    if user:
-        await globalChat.connect(websocket)
-        try:
-            while True:
-                data = await websocket.receive_text()
-                message : Message = store_message(user, data, chatroomID)
-                await globalChat.broadcast(message)
-        except WebSocketDisconnect:
-            globalChat.disconnect(websocket)
-    else:
+    if not user:
+        # TODO Figure out if httpsexceptions are the way to handle bad auth for websockets
         return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials",)
+        
+    await globalChat.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = store_message(user, data, chatroomID)
+            await globalChat.broadcast(message)
+    except WebSocketDisconnect:
+        globalChat.disconnect(websocket)
 
